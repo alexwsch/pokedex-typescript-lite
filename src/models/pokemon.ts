68341456b4309d962b8fd429ee302a002065d033
Alexandre Wetzstein Schumann <alexandrewetzstein@gmail.com>
2026-05-31 13:36:45 -0300

    feat: cria interface PokemonApiResponse

--- src/models/pokemon.ts
+++ src/models/pokemon.ts
@@ -5,3 +5,14 @@ export interface PokemonResumo {
   altura: number;
   peso: number;
 }
+export interface PokemonApiResponse {
+  id: number;
+  name: string;
+  height: number;
+  weight: number;
+  types: {
+    type: {
+      name: string;
+    };
+  }[];
+}
